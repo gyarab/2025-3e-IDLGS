092f275da1d1dd29eef93f7b2ff87aa88288735e
MegapolisPlayer <megapolisplayer@protonmail.com>
2025-12-08 20:37:57 +0100

    folder reorganisation

--- src/demo.spec.ts
+++ src/demo.spec.ts
@@ -1,5 +1,7 @@
 import { describe, it, expect } from 'vitest';
 
+//TODO tests
+
 describe('sum test', () => {
 	it('adds 1 + 2 to equal 3', () => {
 		expect(1 + 2).toBe(3);
--- src/routes/(root)/+layout.svelte
+++ src/routes/(root)/+layout.svelte
@@ -1,17 +1,15 @@
 <script lang="ts">
-	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
-	import 'remixicon/fonts/remixicon.css';
+	import Navbar from './Navbar.svelte';
+	import ChatBot from './components/ChatBot.svelte';
 
 	let { children } = $props();
 </script>
 
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
-
 <div
 	class="flex min-h-screen w-screen max-w-screen min-w-screen grow flex-col overflow-x-hidden overflow-y-scroll"
 >
+	<Navbar />
+	<div class="h-[10lvh] w-full"></div>
 	{@render children?.()}
+	<ChatBot />
 </div>
