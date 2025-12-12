6b9393db14df5dc8d550c9480a149a480201c4c8
Glorious Revolutionary Code Formatter <actions@github.com>
2025-12-12 22:19:06 +0000

    [AUTOMATIC] Code formatting

--- src/routes/(root)/(home)/home/+page.server.ts
+++ src/routes/(root)/(home)/home/+page.server.ts
@@ -1,7 +1,7 @@
 import { db } from '$lib/server/db/index.js';
 import * as dataSchema from '$lib/server/db/schema.js';
 import { fail, redirect } from '@sveltejs/kit';
-import { getUser }from "$lib/server/user/index.js"
+import { getUser } from '$lib/server/user/index.js';
 
 export const load = async (event) => {
 	if (!(await event.parent()).user) {
@@ -11,20 +11,20 @@ export const load = async (event) => {
 	return {
 		//TODO fix and abstract to loadCourses function
 		courses: await db.select().from(dataSchema.course)
-	}
+	};
 };
 
 export const actions = {
 	createTextbook: async (event) => {
 		const user = await getUser(event);
-		if(!user) return fail(401);
+		if (!user) return fail(401);
 	},
 	copyTextbook: async (event) => {
 		const user = await getUser(event);
-		if(!user) return fail(401);
+		if (!user) return fail(401);
 	},
 	createCourse: async (event) => {
 		const user = await getUser(event);
-		if(!user) return fail(401);
+		if (!user) return fail(401);
 	}
 };
