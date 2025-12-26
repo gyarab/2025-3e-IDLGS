54b4bf4c184784df461dc09e8c90a4d11bfb2acd
MegapolisPlayer <megapolisplayer@protonmail.com>
2025-12-26 14:52:36 +0100

    some small edits
    work on #38
    close #39

--- src/routes/(root)/navbar/Footer.svelte
+++ src/routes/(root)/navbar/Footer.svelte
@@ -2,7 +2,7 @@
 	import { m } from '$lib/paraglide/messages';
 </script>
 
-<div class="flex min-h-[20vh] flex-col items-center bg-black p-5 max-lg:hidden">
+<div class="print:hidden flex min-h-[20vh] flex-col items-center bg-black p-5 max-lg:hidden">
 	<div class="grid w-8/10 grid-cols-2 gap-10 *:flex *:h-full *:grow *:flex-col *:gap-2">
 		<div>
 			<div class="flex w-full flex-row items-end gap-2">
@@ -11,7 +11,7 @@
 					{m.textbookNameLong()}
 				</p>
 			</div>
-			<div>© Martin Bykov, Ali Yunussov, Kira Stepanova 2025-2026</div>
+			<div>© Martin Bykov, Kira Stepanova, 2025-{new Date().getFullYear()}</div>
 			<div>
 				{m.allRightsReserved()}
 			</div>
--- src/routes/(root)/navbar/FooterMobile.svelte
+++ src/routes/(root)/navbar/FooterMobile.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
 </script>
 
-<div></div>
+<div class="print:hidden grid grid-cols-2 w-full">
+	<div></div>
+	<div></div>
+</div>
