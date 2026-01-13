1c581bae9156cd0d5d9dd5fdf65995a45d434023
Glorious Revolutionary Code Formatter <actions@github.com>
2026-01-13 00:11:28 +0000

    [AUTOMATIC] Code formatting

--- src/routes/(root)/components/SuccessBox.svelte
+++ src/routes/(root)/components/SuccessBox.svelte
@@ -2,6 +2,5 @@
 	import InfoBox from './InfoBox.svelte';
 </script>
 
-<InfoBox
-	cssClass="bg-linear-to-tr from-green-900 to-green-600 text-white p-2"
+<InfoBox cssClass="bg-linear-to-tr from-green-900 to-green-600 text-white p-2"
 ></InfoBox>
