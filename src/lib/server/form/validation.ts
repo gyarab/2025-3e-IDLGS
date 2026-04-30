6b0d4899eb8469a9e1a4842b64ad13f08cd47cd7
martinbykov <megapolisplayer@protonmail.com>
2026-04-30 22:11:45 +0200

    settings form actions

---
+++ src/lib/server/form/validation.ts
@@ -0,0 +1,7 @@
+import type { ActionFailure, RequestEvent } from "@sveltejs/kit";
+import { isActionFailure } from "@sveltejs/kit";
+
+//TODO
+export const formRunner = async (event: RequestEvent, f: () => Promise<void>): Promise<void | ActionFailure> => {
+
+};
---
+++ src/routes/(protected)/settings/+page.server.ts
@@ -0,0 +1,21 @@
+import { formRunner } from '$lib/server/form/validation';
+
+export const load = async (event) => {};
+
+export const actions = {
+	updatePersonalInfo: async (event) => {
+
+	},
+	updateDescription: async (event) => {
+
+	},
+	updatePassword: async (event) => {
+
+	},
+	updateColor: async (event) => {
+
+	},
+	updateProfilePicture: async (event) => {
+
+	},
+};
