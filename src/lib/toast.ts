3d0c1e698fdb1a2867d7891cdcd371161c7bdecb
IDLGS <actions@github.com>
2026-05-03 19:48:11 +0000

    [AUTOMATIC] Code formatting

--- src/lib/toast.ts
+++ src/lib/toast.ts
@@ -1,18 +1,18 @@
 import { writable } from 'svelte/store';
 
 export interface Toast {
-  id: number;
-  message: string;
-  type: 'error' | 'success' | 'info';
+	id: number;
+	message: string;
+	type: 'error' | 'success' | 'info';
 }
 
 export const toasts = writable<Toast[]>([]);
 
 export const addToast = (message: string, type: Toast['type'] = 'info') => {
-  const id = Date.now();
-  toasts.update((all) => [{ id, message, type }, ...all]);
+	const id = Date.now();
+	toasts.update((all) => [{ id, message, type }, ...all]);
 
-  setTimeout(() => {
-    toasts.update((all) => all.filter((t) => t.id !== id));
-  }, 4000);
+	setTimeout(() => {
+		toasts.update((all) => all.filter((t) => t.id !== id));
+	}, 4000);
 };
