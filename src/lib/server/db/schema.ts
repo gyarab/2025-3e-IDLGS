fa13b2331409484fd1e49200fec6a4ae718c2da1
martinbykov <megapolisplayer@protonmail.com>
2026-05-04 20:57:21 +0200

    kiras text and more documentation work
    Co-authored-by: KiraStepanova <kira.stepanova.s@gyarab.cz>

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,11 +1,13 @@
 import * as user from './schema/userSchema';
 import * as exercise from './schema/exerciseSchema';
 import * as textbook from './schema/textbookSchema';
+import * as book from './schema/bookSchema';
 
 export const schema = {
 	...user,
 	...exercise,
 	...textbook,
+	...book
 };
 
 export default schema;
